5d8fc0ab6989a606d33ea6310aec132ff643d5c3
Cristhian Fuertes <cristhian.fuertes@correounivalle.edu.co>
2017-01-27 10:15:18 -0500

    A bunch of fixes for MovieService and searches

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,149 +8,150 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  baseUrl = "http://image.tmdb.org/t/p/w300/";
+  baseUrl = 'http://image.tmdb.org/t/p/w300/';
   resultsPersons = [];
   resultsMovies = [];
-  search:string;
-  active:boolean=false;
-  focus:boolean=false;
-  colorsPersons:Array<string>=[];
-  colorsMovies:Array<string>=[];
-  color:string="#FFFFFF";
-  totalPages:number;
-  @Input() preview:boolean;
+  search_phrase: string;
+  active: boolean = false;
+  focus: boolean = false;
+  colorsPersons: Array<string> = [];
+  colorsMovies: Array<string> = [];
+  color: string = '#FFFFFF';
+  totalPages: number;
+  @Input() preview: boolean;
 
-  constructor(private movieService: MovieService, private route: ActivatedRoute,
-  		private router: Router){
-  		  this.router.events.subscribe(path => {
-  		    this.clear();
-      });
-  	}
-
-  ngOnInit(){
-
+  public constructor(private movieService: MovieService,
+                     private route: ActivatedRoute,
+                     private router: Router) {
+    this.router.events.subscribe(path => {
+      this.clear();
+    });
   }
-  
+
+  public ngOnInit() {}
+
   /**
   * Change the state of box search
   */
-  changeState(active:boolean){
-    this.active=active;
+  public changeState(active: boolean) {
+    this.active = active;
   }
-  
+
   /**
   * Change the state of input search
   */
-  changeFocus(focus:boolean){
-    this.focus=focus;
+  public changeFocus(focus: boolean) {
+    this.focus = focus;
   }
-  
+
   /**
   * Shade background of "see all results"
   */
-  putColor(){
-    this.color="#C6DEFF";
+  public putColor() {
+    this.color = '#C6DEFF';
   }
-  
+
   /**
   * clear background of "see all results"
   */
-  removeColor(){
-    this.color="#FFFFFF";
+  public removeColor() {
+    this.color = '#FFFFFF';
   }
-  
+
   /**
   * Shade background of result of person
   */
-  putColorPersons(i:number){
-    this.colorsPersons[i]="#C6DEFF";
+  public putColorPersons(i: number) {
+    this.colorsPersons[i] = '#C6DEFF';
   }
-  
+
   /**
   * Shade background of result of movie
   */
-  putColorMovies(i:number){
-    this.colorsMovies[i]="#C6DEFF";
+  public putColorMovies(i: number) {
+    this.colorsMovies[i] = '#C6DEFF';
   }
-  
+
   /**
   * Clear background of result of person
   */
-  removeColorPersons(i:number){
-    this.colorsPersons[i]="#FFFFFF";
+  public removeColorPersons(i: number) {
+    this.colorsPersons[i] = '#FFFFFF';
   }
-  
+
   /**
   * Clear background of result of movie
   */
-  removeColorMovies(i:number){
-    this.colorsMovies[i]="#FFFFFF";
+  public removeColorMovies(i: number) {
+    this.colorsMovies[i] = '#FFFFFF';
   }
-  
+
   /**
   * Reset variables
   */
-  clear(){
-    this.resultsPersons=[];
-    this.resultsMovies=[];
-    this.search="";
-    this.active=false;
+  public clear() {
+    this.resultsPersons = [];
+    this.resultsMovies = [];
+    this.search_phrase = '';
+    this.active = false;
   }
-  
+
   /**
   * Performs the search of persons and movies
   */
-  doSearch(){
-    if(this.search.length >= 2){
-      this.movieService.getSearchPerson(this.search, 1)
-      .subscribe(resultsPersons => {
-        this.resultsPersons=resultsPersons.results.slice(0,3);
-        for(let i=0;i<this.resultsPersons.length;i++){
-          this.colorsPersons[i]="#FFFFFF";
-        }
-      });
-      this.movieService.getSearchMovie(this.search, 1)
-      .subscribe(resultsMovies => {
-        this.resultsMovies=resultsMovies.results.slice(0,3);
-        for(let i=0;i<this.resultsPersons.length;i++){
-          this.colorsMovies[i]="#FFFFFF";
-        }
-      });
-      
-      
-    }else{
-      this.resultsPersons=[];
-       this.resultsMovies=[];
+  public doSearch() {
+    if (this.search_phrase.length >= 2) {
+      this.movieService.searchPerson(this.search_phrase)
+        .subscribe(resultsPersons => {
+          this.resultsPersons = resultsPersons.results.slice(0, 3);
+          for (let i = 0; i < this.resultsPersons.length; i++) {
+            this.colorsPersons[i] = '#FFFFFF';
+          }
+        });
+      this.movieService.searchMovie(this.search_phrase)
+        .subscribe(resultsMovies => {
+          this.resultsMovies = resultsMovies.results.slice(0, 3);
+          for (let i = 0; i < this.resultsPersons.length; i++) {
+            this.colorsMovies[i] = '#FFFFFF';
+          }
+        });
+
+
+    } else {
+      this.resultsPersons = [];
+      this.resultsMovies = [];
     }
   }
-  
+
   /**
-  * Get the Total Url of image
-  * @param {String} Src of image
-  * @return {String} Total Url of image
+  * Get the complete Url of image
+  * @param {String} src the source of the  image
+  * @return {String} complete Url of  the image
   */
-  getUrl(src: string): string {
+  public getUrl(src: string): string {
     return `${this.baseUrl}${src}`;
   }
-  
+
   /**
   * Redirect to a person
+  * @param {Integer} id the person's id
   */
-  goPerson(id:number) {
-    this.router.navigate(['/person/'+id]);
+  public goPerson(id: number) {
+    this.router.navigate(['/person/' + id]);
   }
-  
+
   /**
   * Redirect to a movie
+  * @param {Integer} id the movie's id
   */
-  goMovie(id:number) {
-    this.router.navigate(['/movie/'+id]);
+  public goMovie(id: number) {
+    this.router.navigate(['/movie/' + id]);
   }
-  
+
   /**
   * Redirect to expanded search
   */
-  goExpanded(){
-    this.router.navigate(['/searchexpanded/persons/'+this.search+'/1']);
+  public goExpanded() {
+    this.router.navigate(['/searchexpanded/persons/' + this.search_phrase + '/1']);
   }
 }
