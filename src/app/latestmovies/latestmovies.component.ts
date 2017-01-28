9c5e7a82ae243e228a10f14dc07576bf63f5c888
Cristhian Fuertes <cristhian.fuertes@correounivalle.edu.co>
2017-01-27 22:29:44 -0500

    Refactoring LatestMovies component

--- src/app/latestmovies/latestmovies.component.ts
+++ src/app/latestmovies/latestmovies.component.ts
@@ -5,56 +5,66 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-latestmovies',
   templateUrl: './latestmovies.component.html',
-  styleUrls: ['./latestmovies.component.css']
+  styleUrls: ['./latestmovies.component.less']
 })
+/* This class needs to be deleted ? */
 export class LatestmoviesComponent implements OnInit {
-  baseUrl:string = "http://image.tmdb.org/t/p/w300/";
-  movies:any = [];
+  /**
+  * Base Url for the movie's images
+  */
+  baseUrl: string = 'http://image.tmdb.org/t/p/w300/';
+  /**
+  * List of movies
+  */
+  movies: any = [];
+  /**
+  * Flag to preview in the toolbar
+  */
   @Input() preview: boolean = false;
 
-  constructor(private movieService: MovieService, private route: ActivatedRoute,
-  		private router: Router){}
+  public constructor(private movieService: MovieService,
+                     private route: ActivatedRoute,
+                     private router: Router) { }
 
   /**
   * Get the latest movies
   */
-  ngOnInit(){
+  public ngOnInit() {
     this.movieService.getLatest()
       .subscribe(movies => {
         if (this.preview) {
           try {
-             this.movies = movies.slice(0,4);
-          }
-          catch(err){
+             this.movies = movies.slice(0, 4);
+          } catch (err) {
             this.movies = movies;
           }
-        }
-        else {
+        } else {
           this.movies = movies;
         }
       });
   }
 
   /**
-  * Get the Total Url of image
-  * @param {String} Src of image
-  * @return {String} Total Url of image
+  * Get the complete Url of the image
+  * @param {String} src the source of the image
+  * @return {String} The complete Url of image
   */
-  getUrl(src: string): string {
+  public getUrl(src: string): string {
     return `${this.baseUrl}${src}`;
   }
-  
+
   /**
   * Redirect to latestmovies component in total window
   */
-  goLatestMovies() {
+  public goLatestMovies() {
     this.router.navigate(['/latestmovies']);
   }
-  
+
   /**
   * Redirect to a movie
+  * @param {Integer} id The movie's id
   */
-  goMovie(id:number) {
-    this.router.navigate(['/movie/'+id]);
+  public goMovie(id: number) {
+    this.router.navigate(['/movie/' + id]);
   }
 }
