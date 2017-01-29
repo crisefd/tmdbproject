2c64976abdbe45232faef207a250f1a259b240bb
Cristhian Fuertes <cristhian.fuertes@correounivalle.edu.co>
2017-01-29 17:20:59 -0500

    Refactoring MovieCrew Component

--- src/app/moviecrew/moviecrew.component.ts
+++ src/app/moviecrew/moviecrew.component.ts
@@ -4,119 +4,137 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-moviecrew',
   templateUrl: './moviecrew.component.html',
-  styleUrls: ['./moviecrew.component.css']
+  styleUrls: ['./moviecrew.component.less']
 })
 export class MoviecrewComponent implements OnInit, OnChanges {
-  baseUrl = "http://image.tmdb.org/t/p/w300/";
+  /**
+  * Image's base Url
+  */
+  baseUrl = 'http://image.tmdb.org/t/p/w300/';
+  /**
+  * Movie's credits list
+  */
   @Input() credits: any;
-  copyCredits:any;
-  active:boolean = false;
-  orderUp:boolean = false;
-  parameter:string ="Relevance";
-  borderColorRelevance:string="#FFC107";
-  borderColorAlphabetic:string="#673ab7";
 
-  constructor(private route: ActivatedRoute,
-  		private router: Router){}
+  copyCredits: any;
+  /**
+  * Flag for showing credits
+  */
+  active: boolean = false;
+  /**
+  * Flag for ordering credits
+  */
+  orderUp: boolean = false;
+  /**
+  * Type of ordering
+  */
+  parameter: string = 'Relevance';
+  /**
+  * Border color with Relevance ordering
+  */
+  borderColorRelevance: string = '#FFC107';
+  /**
+  * Border color with Alphabetic ordering
+  */
+  borderColorAlphabetic: string = '#673ab7';
 
-  ngOnInit(){
-  }
+  public constructor(private route: ActivatedRoute,
+                     private router: Router) { }
+
+  public ngOnInit() { }
 
   /**
-  * Get the Total Url of image
-  * @param {String} Src of image
-  * @return {String} Total Url of image
+  * Get the Complete Url of the image
+  * @param {String} src Source of the image
+  * @return {String} Complete Url of the image
   */
-  getUrl(src: string): string {
+  public getUrl(src: string): string {
     return `${this.baseUrl}${src}`;
   }
-  
+
   /**
   * Change visibility of component in the html
   */
-  changeState() {
-    this.active=!this.active;
+  public changeState() {
+    this.active = !this.active;
   }
-  
+
   /**
   * Makes relevance the new ordering parameter
   */
-  changeParameterRelevance() {
-    this.borderColorRelevance="#FFC107";
-    this.borderColorAlphabetic="#673ab7";
-    this.parameter="Relevance";
+  public changeParameterRelevance() {
+    this.borderColorRelevance = '#FFC107';
+    this.borderColorAlphabetic = '#673ab7';
+    this.parameter = 'Relevance';
     this.doSort();
-  }  
-  
+  }
+
   /**
   * Makes alphabetic the new ordering parameter
   */
-  changeParameterAlphabetic() {
-    this.borderColorAlphabetic="#FFC107";
-    this.borderColorRelevance="#673ab7";
-    this.parameter="Alphabetic";
+  public changeParameterAlphabetic() {
+    this.borderColorAlphabetic = '#FFC107';
+    this.borderColorRelevance = '#673ab7';
+    this.parameter = 'Alphabetic';
     this.doSort();
-  }  
-  
+  }
+
   /**
   * Reverse order
   */
-  changeOrder() {
-    this.orderUp=!this.orderUp;
+  public changeOrder() {
+    this.orderUp = !this.orderUp;
     this.doSort();
   }
-  
+
   /**
   * Restart the component
   */
-  ngOnChanges(changes){
-    this.copyCredits=this.credits;
+  public ngOnChanges(changes) {
+    this.copyCredits = this.credits;
     this.active = false;
     this.orderUp = false;
-    this.parameter ="Relevance";
-    this.borderColorRelevance="#FFC107";
-    this.borderColorAlphabetic="#673ab7";
+    this.parameter = 'Relevance';
+    this.borderColorRelevance = '#FFC107';
+    this.borderColorAlphabetic = '#673ab7';
     this.doSort();
   }
-  
+
   /**
   * Performs the ordering
   */
-  doSort(){
-    
-    if(this.parameter=="Relevance"){
-      if(this.orderUp){
-        console.log();
-        this.credits=this.copyCredits.slice().reverse();
-      }
-      else{
-        this.credits=this.copyCredits.slice();
+  public doSort() {
 
+    if (this.parameter === 'Relevance') {
+      if (this.orderUp) {
+        this.credits = this.copyCredits.slice().reverse();
+      } else {
+        this.credits = this.copyCredits.slice();
       }
     }
-    
-    if(this.parameter=="Alphabetic"){
-      if(this.orderUp){
-        this.credits.sort(function(a,b) {
-          var x = a.name.toLowerCase();
-          var y = b.name.toLowerCase();
+
+    if (this.parameter === 'Alphabetic') {
+      if (this.orderUp) {
+        this.credits.sort(function(a, b) {
+          let x = a.name.toLowerCase();
+          let y = b.name.toLowerCase();
           return x < y ? -1 : x > y ? 1 : 0;
         });
-      }
-      else{
-        this.credits.sort(function(a,b) {
-          var x = a.name.toLowerCase();
-          var y = b.name.toLowerCase();
+      } else {
+        this.credits.sort(function(a, b) {
+          let x = a.name.toLowerCase();
+          let y = b.name.toLowerCase();
           return y < x ? -1 : y > x ? 1 : 0;
         });
       }
     }
   }
-  
+
   /**
   * Redirect to a person
+  * @param {Integer} id person's id
   */
-  goPerson(id:number) {
-    this.router.navigate(['/person/'+id]);
+  goPerson(id: number) {
+    this.router.navigate(['/person/' + id]);
   }
 }
